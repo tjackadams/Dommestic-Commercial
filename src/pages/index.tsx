1ade16c0c5d905ce2a03f9879eb957caee9c8107
Thomas Adams <tjackadams@itadams.co.uk>
2019-09-10 16:05:39 +0100

    working on the header styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,66 +1,128 @@
 import React from "react"
-import { Stack, Text, Link, FontWeights } from "office-ui-fabric-react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+import {
+  DefaultPalette,
+  FontSizes,
+  FontWeights,
+  Link,
+  Overlay,
+  Stack,
+  Text,
+  ITextStyles,
+} from "office-ui-fabric-react"
+
 import "./index.css"
 
-const boldStyle = { root: { fontWeight: FontWeights.semibold } }
+const linkStyles: ITextStyles = {
+  root: {
+    color: DefaultPalette.neutralDark,
+    cursor: "pointer",
+    fontSize: FontSizes.large,
+    fontWeight: FontWeights.semilight,
+    border: "1px solid transparent",
+    selectors: {
+      ":hover": {
+        color: DefaultPalette.neutralPrimaryAlt,
+        borderBottom: `1px solid ${DefaultPalette.neutralPrimaryAlt}`,
+      },
+    },
+  },
+}
 
-const App: React.FunctionComponent = () => {
+const App = props => {
+  console.log("props: ", props)
   return (
-    <Stack
-      horizontalAlign="center"
-      verticalAlign="center"
-      verticalFill
-      styles={{
-        root: {
-          width: "960px",
-          margin: "0 auto",
-          textAlign: "center",
-          color: "#605e5c",
-        },
-      }}
-      className="App"
-      gap={15}
-    >
-      <img
-        src="https://raw.githubusercontent.com/Microsoft/just/master/packages/just-stack-uifabric/template/src/components/fabric.png"
-        alt="logo"
-      />
-      <Text variant="xxLarge" styles={boldStyle}>
-        Welcome to Your UI Fabric App
-      </Text>
-      <Text variant="large">
-        For a guide on how to customize this project, check out the UI Fabric
-        documentation.
-      </Text>
-      <Text variant="large" styles={boldStyle}>
-        Essential Links
-      </Text>
-      <Stack horizontal gap={15} horizontalAlign="center">
-        <Link href="https://developer.microsoft.com/en-us/fabric">Docs</Link>
-        <Link href="https://stackoverflow.com/questions/tagged/office-ui-fabric">
-          Stack Overflow
-        </Link>
-        <Link href="https://github.com/officeDev/office-ui-fabric-react/">
-          Github
-        </Link>
-        <Link href="https://twitter.com/officeuifabric">Twitter</Link>
-      </Stack>
-      <Text variant="large" styles={boldStyle}>
-        Design System
-      </Text>
-      <Stack horizontal gap={15} horizontalAlign="center">
-        <Link href="https://developer.microsoft.com/en-us/fabric#/styles/icons">
-          Icons
-        </Link>
-        <Link href="https://developer.microsoft.com/en-us/fabric#/styles/typography">
-          Typography
-        </Link>
-        <Link href="https://developer.microsoft.com/en-us/fabric#/styles/themegenerator">
-          Theme
-        </Link>
-      </Stack>
+    <Stack>
+      <Stack.Item
+        styles={{
+          root: { backgroundColor: DefaultPalette.white, padding: 10 },
+        }}
+      >
+        <Stack
+          horizontal
+          verticalAlign="center"
+          tokens={{ childrenGap: 14 }}
+          styles={{ root: { minHeight: 60 } }}
+        >
+          <Stack.Item grow={1}> </Stack.Item>
+          <Stack.Item>
+            <Text
+              variant="xLarge"
+              as="h1"
+              styles={{ root: { fontWeight: FontWeights.semibold } }}
+            >
+              Domestic &#38; Commercial Drain Services
+            </Text>
+          </Stack.Item>
+          <Stack.Item>
+            <Text variant="xLarge"> | </Text>
+          </Stack.Item>
+          <Stack.Item styles={{ root: { marginRight: 60 } }}>
+            <Text
+              variant="xLarge"
+              as="h1"
+              styles={{ root: { fontWeight: FontWeights.semibold } }}
+            >
+              Dudley
+            </Text>
+          </Stack.Item>
+          <Stack.Item grow>
+            <Stack
+              horizontal
+              verticalAlign="center"
+              tokens={{ childrenGap: 20 }}
+              styles={{ root: { paddingTop: 4 } }}
+            >
+              <Stack.Item>
+                <Link href="/">
+                  <Text styles={linkStyles}>Home</Text>
+                </Link>
+              </Stack.Item>
+              <Stack.Item>
+                <Link href="/">
+                  <Text styles={linkStyles}>Services</Text>
+                </Link>
+              </Stack.Item>
+              <Stack.Item>
+                <Link href="/">
+                  <Text styles={linkStyles}>Contact</Text>
+                </Link>
+              </Stack.Item>
+            </Stack>
+          </Stack.Item>
+          <Stack.Item grow={2}></Stack.Item>
+        </Stack>
+      </Stack.Item>
+      <Stack.Item>
+        <Img fluid={props.data.drainBanner.childImageSharp.fluid} />
+        <Overlay>
+          <Text>overlay</Text>
+        </Overlay>
+      </Stack.Item>
+      <Stack.Item
+        styles={{
+          root: {
+            backgroundColor: DefaultPalette.white,
+            maxWidth: 800,
+            marginTop: 20,
+          },
+        }}
+      ></Stack.Item>
     </Stack>
   )
 }
 
 export default App
+
+export const query = graphql`
+  query {
+    drainBanner: file(relativePath: { eq: "drain-banner.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 3840) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
