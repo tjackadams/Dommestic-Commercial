d50ede4f4227b63b823f0324c3a806f7e52504e5
Thomas Adams <tjackadams@itadams.co.uk>
2022-01-14 14:14:42 +0000

    update footer to match current content.

--- components/footer.tsx
+++ components/footer.tsx
@@ -11,67 +11,68 @@ export default function Footer() {
       <div className="w-100" style={{ height: 600 }}>
         <Map openingTimes={openingTimes} />
       </div>
-      <Container
-        fluid
-        className="text-white p-4"
-        style={{ backgroundColor: "#323130" }}
-      >
-        <Row md={2}>
-          <Col>
-            <p className="fw-bold">Domestic & Commercial Drain Services</p>
-            <ul className="list-unstyled">
-              <li>67 Merryfield Road</li>
-              <li>Dudley</li>
-              <li>West Midlands</li>
-              <li>DY1 2ND</li>
-            </ul>
-            <ul className="list-unstyled text-primary lh-lg">
-              <li>
-                <a className="text-decoration-none" href="tel:01384357446">
-                  <i className="bi bi-telephone"></i> 01384 357446
-                </a>
-              </li>
-              <li>
-                <a className="text-decoration-none" href="tel:07974243764">
-                  <i className="bi bi-telephone"></i> 07974 243764
-                </a>
-              </li>
-              <li>
-                <a
-                  className="text-decoration-none"
-                  href="mailto:stevetomkins53@gmail.com"
-                >
-                  <i className="bi bi-envelope"></i> stevetomkins53@gmail.com
-                </a>
-              </li>
-            </ul>
-          </Col>
-          <Col>
-            <p>Opening Times</p>
-            <ul className="list-unstyled lh-base">
-              {openingTimes &&
-                openingTimes.map((openingTime) => {
-                  return (
-                    <li>
-                      {openingTime.day} {openingTime.opens}
-                      {openingTime.opens !== "Closed"
-                        ? " - " + openingTime.closes
-                        : ""}
-                    </li>
-                  );
-                })}
-            </ul>
-          </Col>
-        </Row>
-        <Row md={2}>
-          <Col>
-            <p>© 2022 - Domestic & Commercial Drain Services</p>
-          </Col>
-          <Col>
-            <p>Website design by ITadams</p>
-          </Col>
-        </Row>
-      </Container>
+      <div className="d-flex flex-wrap justify-content-evenly align-items-start text-white p-4 bg-dark">
+        <div className="flex-shrink-1">
+          <p className="fw-bold">Domestic & Commercial Drain Services</p>
+          <ul className="list-unstyled">
+            <li>67 Merryfield Road</li>
+            <li>Dudley</li>
+            <li>West Midlands</li>
+            <li>DY1 2ND</li>
+          </ul>
+          <ul className="list-unstyled lh-lg">
+            <li>
+              <a
+                className="link-primary text-decoration-none"
+                href="tel:01384357446"
+              >
+                <i className="bi bi-telephone"></i> 01384 357446
+              </a>
+            </li>
+            <li>
+              <a
+                className="link-primary text-decoration-none"
+                href="tel:07974243764"
+              >
+                <i className="bi bi-telephone"></i> 07974 243764
+              </a>
+            </li>
+            <li>
+              <a
+                className="link-primary text-decoration-none"
+                href="mailto:stevetomkins53@gmail.com"
+              >
+                <i className="bi bi-envelope"></i> stevetomkins53@gmail.com
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="vr d-none d-md-block"></div>
+        <div className="flex-shrink-1">
+          <p className="fw-bold">Opening Times</p>
+          <ul className="list-unstyled lh-lg">
+            {openingTimes &&
+              openingTimes.map((openingTime) => {
+                return (
+                  <li>
+                    {openingTime.day} {openingTime.opens}
+                    {openingTime.opens !== "Closed"
+                      ? " - " + openingTime.closes
+                      : ""}
+                  </li>
+                );
+              })}
+          </ul>
+        </div>
+      </div>
+      <div className="d-flex flex-nowrap justify-content-evenly align-items-center text-white p-2 bg-dark">
+        <div>
+          <p>© 2022 - Domestic & Commercial Drain Services</p>
+        </div>
+        <div>
+          <p>Website design by ITadams</p>
+        </div>
+      </div>
     </footer>
   );
 }
