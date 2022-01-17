5d8d19220a4c575698fbee1dc28689323a1344ef
Thomas Adams <tjackadams@itadams.co.uk>
2022-01-17 17:11:51 +0000

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,52 @@
 import { DefaultSeo, LocalBusinessJsonLd } from "next-seo";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { useState } from "react";
+import { useRouter } from "next/router";
+import Script from "next/script";
+import { useEffect, useState } from "react";
 import { SSRProvider } from "react-bootstrap";
 import AppContext from "../appContext";
 import Layout from "../components/layout";
 import { OpeningTime } from "../configuration/opening-times";
-
+import * as gtag from "../lib/gtag";
 import "../styling/app.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   const [openingTimes, setOpeningTimes] = useState<OpeningTime[]>([]);
 
   const value = { openingTimes, setOpeningTimes };
 
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Head>
         <link rel="shortcut icon" href="/favicon.png" />
       </Head>
